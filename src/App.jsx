dcd1d1737f6c83fd9449e859b3e7c5eb47ea0b6f
Abhishek kumar <13abhi03@gmail.com>
2024-04-13 20:32:40 +0530

    signup-form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Signup from './Component/Signup.jsx'
+
+function App() {
+  return(
+    <Signup/>
+  )
+  
+}
+
+export default App
---
+++ src/Component/Signup.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react"
+
+const Signup = () => {
+
+    const[email, setEmail] = useState("");
+    const[password, setPassword]=useState("");
+    const[confirmPassword, setConfirmPassword] = useState("");
+
+    const[emailError,setEmailError]=useState(false);
+    const[passError,setPassError]=useState(false);
+    const[confirmError,setConfirmError]=useState(false);
+
+    const handleEmail = (e) =>{
+        let mail = e.target.value;
+        let cond1 = mail.length>3;
+        let cond2 = mail.indexOf('@')!=-1;
+        let cond3 = mail.indexOf('.')!=-1;
+        if(cond1 && cond2 && cond3){
+            console.log("valid")
+            setEmailError(false);
+        }else{
+            setEmailError(true)
+        }
+        setEmail(mail);
+    }
+
+    const handlePassword = (e) =>{
+        if((e.target.value).length < 8)setPassError(true);
+        else setPassError(false);
+        setPassword(e.target.value);
+    }
+
+    const confirmHandler = (e) =>{
+        if(password===e.target.value)setConfirmError(false);
+        else setConfirmError(true);
+        setConfirmPassword(e.target.value);
+    }
+
+    const submitHandler = (e) =>{
+        e.preventDefault();
+        if(emailError || passError || confirmError)alert("Can’t submit the form")
+        else{ 
+            setEmail("");
+            setPassword("");
+            setConfirmPassword("");
+            alert('Form submitted successfully');
+        }
+    }
+    console.log("rendered");
+    return(
+        
+        <>
+        <div className="container flex justify-center items-center h-screen">
+            <form action="" className="w-2/5 flex flex-col gap-y-2">
+
+                <label htmlFor="email">Email:</label>
+                <input 
+                    className={`w-full border border-slate-500 rounded-md p-1 focus:outline-blue-600" ${email ?( emailError ? 'border-red-600 outline-red-600' : 'border-2 border-green-700 outline-green-700 '):""}`}
+                    type="email" 
+                    name="email" 
+                    id="email "
+                    value={email}
+                    onChange={handleEmail}/> 
+                {emailError?<div className=" text-red-700">Invalid email format</div>:""}
+
+                <label htmlFor="password">Password:</label>
+                <input 
+                    className={`w-full border border-slate-500 rounded-md p-1 focus:outline-blue-600"  ${password ? ( passError ? 'border-red-600 outline-red-600' : `border-2 border-green-700  outline-green-700`):""} `}
+                    type="password" 
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={handlePassword}/>
+                {passError ? <div className=" text-red-700">Password must be atleast 8 characters</div>:""}
+
+                <label htmlFor="confirm_password">Confirm Password</label>
+                <input 
+                    className={`w-full border border-slate-500 rounded-md p-1 focus:outline-blue-600"  ${confirmPassword ? (confirmError ? 'border-red-600 outline-red-600' : 'border-2 border-green-700  outline-green-700'):""}`} 
+                    type="password" 
+                    name="confirm_password"
+                    id="confirm_password"
+                    value={confirmPassword}
+                    onChange={confirmHandler}/>
+                {confirmError ?<div className=" text-red-700">Passwords do not match</div>:""}
+
+                <button onClick={submitHandler} className="bg-blue-700 w-28 text-white p-1 rounded-md my-1 mx-auto block transition-all hover:scale-110 hover:bg-blue-600" >SignUp</button>
+            </form>
+        </div>
+
+        </>
+    )
+}
+export default Signup
